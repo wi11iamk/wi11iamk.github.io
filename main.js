4d557f33a739c41a8c54445ee0e16a723d87fa50
William Kistler <williamdkistler@gmail.com>
2022-06-13 09:29:20 -0400

    Update main.js
    Added skill.

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
       case "skills":
         clearInput();
-        addTextToResults("Languages: Python, C, Git, Bash<br>Technologies: MEG<br>Software: DeepLabCut, Spyder IDE, Blender, Linux");
+        addTextToResults("Languages: Matlab, Python, C, Git, Bash<br>Technologies: MEG<br>Software: DeepLabCut, Spyder IDE, Blender, Linux");
         break;
         
       case "contact":
