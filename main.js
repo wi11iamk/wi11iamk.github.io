a3acab41263d4f3cdb09101bddb58ad7d906f604
William Kistler <williamdkistler@gmail.com>
2023-02-25 07:08:51 +0000

    Add to skills

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
       case "skills":
         clearInput();
-        addTextToResults("Languages: Matlab, Python, C, Git, Bash<br>Technologies: MEG<br>Software: DeepLabCut, Spyder IDE, Blender, Linux");
+        addTextToResults("Languages: Matlab, Python, C, Git, Bash<br>Technologies: MEG<br>Software: DeepLabCut, Spyder IDE, Blender, Linux, Inkscape");
         break;
         
       case "contact":
