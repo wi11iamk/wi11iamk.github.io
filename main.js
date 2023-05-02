73c74f35bf9a976afbfa5db643f405a0fde54060
William Kistler <williamdkistler@gmail.com>
2023-05-02 10:33:57 -0400

    Added to skills/languages

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
       case "skills":
         clearInput();
-        addTextToResults("Languages: Python, C, Git, Bash<br>Technologies: MEG<br>Software: DeepLabCut, Spyder IDE, Inkscape, Blender, GNU/Linux");
+        addTextToResults("Languages: Python, C, Git, Bash, Cython<br>Technologies: MEG<br>Software: DeepLabCut, Spyder IDE, Inkscape, Blender, GNU/Linux");
         break;
         
       case "contact":
